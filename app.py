f8a8e49dabb0a75f7494bea1b2eb3800d0443568
Chimdalu Ifediba <chimdaluifediba@gmail.com>
2025-12-23 00:48:59 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st # type: ignore
+
+
+st.set_page_config(page_title="Stock Dashboard", layout="wide")
+st.title("Stock Prediction Dashboard")
+st.write("Use the left sidebar to navigate pages (Model / Sentiment).")
