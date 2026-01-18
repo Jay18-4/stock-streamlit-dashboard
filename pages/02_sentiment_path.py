eb6e5db192e7ec2e5951e65e4f0ed3e1ddd16c5f
Chimdalu Ifediba <chimdaluifediba@gmail.com>
2026-01-18 04:54:24 +0100

    Update 02_sentiment_path.py

--- pages/02_sentiment_path.py
+++ pages/02_sentiment_path.py
@@ -1,13 +1,15 @@
 import streamlit as st # type: ignore
 import pandas as pd
-from utils.api_client import read_json, daily_cache
+from utils.api_client import read_json, daily_cloud_cache
 
 st.set_page_config(page_title="Sentiment Path", layout="wide")
 
 st.title("📰 News & Sentiment Dashboard")
 
-news_data = daily_cache("news_sentiment", read_json("news_sent.json"))
-
+news_data = daily_cloud_cache(
+    "news_sentiment",
+    lambda: read_json("news_sent.json")
+)
 # Convert to DataFrame for display
 news_df = pd.DataFrame(news_data["news_sent"])
 
@@ -21,4 +23,5 @@ st.metric("Polarity Sum", news_df["pol_sum"].sum())
 st.metric("Positive Count", news_df["pos_count"].sum())
 st.metric("Negative Count", news_df["neg_count"].sum())
 st.metric("Neu Sentiment", news_df["neu_count"].sum())
+
 
