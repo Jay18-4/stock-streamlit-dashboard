895334f140f4570c99d36aab202fe2c3229fd802
Chimdalu Ifediba <chimdaluifediba@gmail.com>
2026-01-18 04:53:00 +0100

    Replace daily_cache with daily_cloud_cache

--- pages/01_model_path.py
+++ pages/01_model_path.py
@@ -1,6 +1,6 @@
 import streamlit as st # type: ignore
 import pandas as pd
-from utils.api_client import read_csv, read_json, daily_cache
+from utils.api_client import read_csv, read_json, daily_cloud_cache
 
 
 st.set_page_config(page_title="Model Path", layout="wide")
@@ -8,8 +8,14 @@ st.set_page_config(page_title="Model Path", layout="wide")
 st.title("📈 Model Path Dashboard")
 
 # ---- Fetch data with daily caching ----
-latest_data = daily_cache("latest_data", read_json("latest_data_snapshot.json"))
-predictions = daily_cache("predictions", read_csv("prediction_history.csv"))
+latest_data = daily_cloud_cache(
+    "latest_data",
+    lambda: read_json("latest_data_snapshot.json")
+)
+predictions = daily_cloud_cache(
+    "predictions",
+    lambda: read_json("prediction_history.csv")
+)
 
 # ---- Convert stocks to DataFrame ----
 stocks_df = pd.DataFrame(latest_data["stocks"])
@@ -32,4 +38,5 @@ for ticker,pred in zip(merged_df['Ticker'], merged_df['Prediction']):
 # ---- Optional: Plot Close prices ----
 st.subheader("Stock Close Prices")
 st.line_chart(stocks_df.set_index("Ticker")["Close"])
+
 
